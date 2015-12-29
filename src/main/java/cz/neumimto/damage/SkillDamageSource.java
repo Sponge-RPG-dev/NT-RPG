556b309064fa4e96a507b48d8e9223402025b662
NeumimTo <chleba48@gmail.com>
2015-12-29 11:54:51 +0100

    no more npes on damage

--- src/main/java/cz/neumimto/damage/SkillDamageSource.java
+++ src/main/java/cz/neumimto/damage/SkillDamageSource.java
@@ -1,77 +1,50 @@
-/*  Copyright (c) 2015, NeumimTo https://github.com/NeumimTo
-*
-*     This program is free software: you can redistribute it and/or modify
-*     it under the terms of the GNU General Public License as published by
-*     the Free Software Foundation, either version 3 of the License, or
-*     (at your option) any later version.
-*
-*     This program is distributed in the hope that it will be useful,
-*     but WITHOUT ANY WARRANTY; without even the implied warranty of
-*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*     GNU General Public License for more details.
-*
-*     You should have received a copy of the GNU General Public License
-*     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+/*
+ *     Copyright (c) 2015, NeumimTo https://github.com/NeumimTo
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package cz.neumimto.damage;
 
 import cz.neumimto.players.IActiveCharacter;
 import cz.neumimto.skills.ISkill;
-import org.spongepowered.api.Sponge;
-import org.spongepowered.api.event.cause.entity.damage.DamageType;
-import org.spongepowered.api.event.cause.entity.damage.DamageTypes;
-import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
-import org.spongepowered.api.event.cause.entity.damage.source.DamageSources;
 import org.spongepowered.api.event.cause.entity.damage.source.common.AbstractDamageSource;
-import org.spongepowered.api.event.cause.entity.damage.source.common.AbstractDamageSourceBuilder;
 
-public class SkillDamageSource extends AbstractDamageSource {
+/**
+ * Created by NeumimTo on 29.12.2015.
+ */
+public class SkillDamageSource extends AbstractDamageSource implements ISkillDamageSource {
+
     private ISkill skill;
     private IActiveCharacter caster;
-    private DamageType damageType = DamageTypes.ATTACK;
 
-    public SkillDamageSource(AbstractDamageSourceBuilder builder) {
+
+    public SkillDamageSource(SkillDamageSourceBuilder builder) {
         super(builder);
+        this.skill = builder.getSkill();
+        this.caster = builder.getCaster();
     }
 
-    public SkillDamageSource() {
-        this(new SkillDamageSourceBuilder());
+    @Override
+    public final ISkill getSkill() {
+        return this.skill;
     }
 
-    public void setSkill(ISkill skill) {
-        this.skill = skill;
-    }
-
-    public void setCaster(IActiveCharacter caster) {
-        this.caster = caster;
-    }
-
-    public void setDamageType(DamageType damageType) {
-        this.damageType = damageType;
-    }
-
-    public ISkill getSkill() {
-        return skill;
-    }
-
+    @Override
     public IActiveCharacter getCaster() {
         return caster;
     }
 
-    @Override
-    public DamageType getType() {
-        return damageType;
-    }
 
-
-    @Override
-    public boolean isScaledByDifficulty() {
-        return false;
-    }
-
-    @Override
-    public boolean doesAffectCreative() {
-        return false;
-    }
 }
--- src/main/java/cz/neumimto/damage/SkillDamageSourceBuilder.java
+++ src/main/java/cz/neumimto/damage/SkillDamageSourceBuilder.java
@@ -1,25 +1,52 @@
+/*
+ *     Copyright (c) 2015, NeumimTo https://github.com/NeumimTo
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package cz.neumimto.damage;
 
-import org.spongepowered.api.event.cause.entity.damage.DamageType;
-import org.spongepowered.api.event.cause.entity.damage.DamageTypes;
-import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
-import org.spongepowered.api.event.cause.entity.damage.source.common.AbstractDamageSource;
+import cz.neumimto.players.IActiveCharacter;
+import cz.neumimto.skills.ISkill;
 import org.spongepowered.api.event.cause.entity.damage.source.common.AbstractDamageSourceBuilder;
-import org.spongepowered.api.util.ResettableBuilder;
 
 /**
- * Created by NeumimTo on 28.12.2015.
+ * Created by NeumimTo on 29.12.2015.
  */
-public class SkillDamageSourceBuilder extends AbstractDamageSourceBuilder {
+public class SkillDamageSourceBuilder extends AbstractDamageSourceBuilder<SkillDamageSource, SkillDamageSourceBuilder> {
 
+    protected ISkill skill;
+    protected IActiveCharacter caster;
 
-    @Override
-    public DamageSource build() throws IllegalStateException {
-        return new SkillDamageSource(this);
+    public ISkill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(ISkill skill) {
+        this.skill = skill;
+    }
+
+    public IActiveCharacter getCaster() {
+        return caster;
+    }
+
+    public void setCaster(IActiveCharacter caster) {
+        this.caster = caster;
     }
 
     @Override
-    public ResettableBuilder from(Object o) {
-        return new SkillDamageSourceBuilder();
+    public SkillDamageSource build() throws IllegalStateException {
+        return new SkillDamageSource(this);
     }
 }
