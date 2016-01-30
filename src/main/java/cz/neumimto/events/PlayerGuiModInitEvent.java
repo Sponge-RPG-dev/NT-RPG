75f57ac7b02402adb665c1eba9b2f92b164061be
NeumimTo <chleba48@gmail.com>
2016-01-30 22:17:45 +0100

    event for gui mod

---
+++ src/main/java/cz/neumimto/events/PlayerGuiModInitEvent.java
@@ -0,0 +1,32 @@
+package cz.neumimto.events;
+
+import org.spongepowered.api.event.Event;
+import org.spongepowered.api.event.cause.Cause;
+
+import java.util.UUID;
+
+/**
+ * Created by NeumimTo on 30.1.2016.
+ */
+public class PlayerGuiModInitEvent implements Event {
+
+    UUID uuid;
+
+    public PlayerGuiModInitEvent(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public Cause getCause() {
+        return Cause.ofNullable(null);
+    }
+
+}
