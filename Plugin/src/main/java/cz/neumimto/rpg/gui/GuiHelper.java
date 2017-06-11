3b56def8dac02a0b527ea5245092a641cc7a9f35
NeumimTo <chleba48@gmail.com>
2017-06-11 16:15:24 +0200

    inventory menu update

--- Plugin/src/main/java/cz/neumimto/rpg/gui/GuiHelper.java
+++ Plugin/src/main/java/cz/neumimto/rpg/gui/GuiHelper.java
@@ -42,7 +42,6 @@ public class GuiHelper {
 		i.query(new SlotPos(3, 2)).offer(createArmorCommand(group));
 		i.query(new SlotPos(2, 3)).offer(createAttributesCommand(group));
 		i.query(new SlotPos(0, 0)).offer(createDescriptionItem(group.getDescription()));
-		i.query(new SlotPos(2, 4)).offer(createInitialAttributesCommand(group));
 		return i;
 	}
 
@@ -66,7 +65,7 @@ public class GuiHelper {
 	}
 
 	public static ItemStack createArmorCommand(PlayerGroup group) {
-		ItemStack i = ItemStack.of(ItemTypes.DIAMOND_SWORD, 1);
+		ItemStack i = ItemStack.of(ItemTypes.DIAMOND_CHESTPLATE, 1);
 		i.offer(NKeys.MENU_INVENTORY, true);
 		i.offer(Keys.DISPLAY_NAME, Text.of(Localization.WEAPONS, TextColors.DARK_RED));
 		i.offer(Keys.ITEM_LORE, Arrays.asList(Text.of(Localization.WEAPONS_MENU_HELP, TextColors.GRAY)));
@@ -77,7 +76,7 @@ public class GuiHelper {
 	}
 
 	public static ItemStack createWeaponCommand(PlayerGroup group) {
-		ItemStack i = ItemStack.of(ItemTypes.DIAMOND_CHESTPLATE, 1);
+		ItemStack i = ItemStack.of(ItemTypes.DIAMOND_SWORD, 1);
 		i.offer(NKeys.MENU_INVENTORY, true);
 		i.offer(Keys.DISPLAY_NAME, Text.of(Localization.ARMOR, TextColors.DARK_RED));
 		i.offer(Keys.ITEM_LORE, Arrays.asList(Text.of(Localization.ARMOR_MENU_HELP, TextColors.GRAY)));
@@ -112,13 +111,6 @@ public class GuiHelper {
 		of.offer(Keys.DISPLAY_NAME, Text.of(Localization.BACK, TextColors.WHITE));
 		String c = IoC.get().build(InfoCommand.class).getAliases().get(0);
 		of.offer(new InventoryCommandItemMenuData(c + " " + l + " " + g.getName()));
-		return of;
-	}
-
-
-	public static ItemStack createInitialAttributesCommand(PlayerGroup group) {
-		ItemStack of = ItemStack.of(ItemTypes.BOOK, 1);
-		of.offer(new InventoryCommandItemMenuData("show attributes-initial " + group.getName()));
 		return of;
 	}
 	
