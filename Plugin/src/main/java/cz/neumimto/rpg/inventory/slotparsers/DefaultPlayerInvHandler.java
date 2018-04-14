7bd7d9aa882c33e043080ca8370c5862eda02b5c
NeumimTo <chleba48@gmail.com>
2018-04-14 10:19:15 +0200

    delete this

--- Plugin/src/main/java/cz/neumimto/rpg/inventory/slotparsers/DefaultPlayerInvHandler.java
+++ Plugin/src/main/java/cz/neumimto/rpg/inventory/slotparsers/DefaultPlayerInvHandler.java
@@ -2,11 +2,6 @@ package cz.neumimto.rpg.inventory.slotparsers;
 
 import cz.neumimto.core.ioc.Singleton;
 import cz.neumimto.rpg.NtRpgPlugin;
-import cz.neumimto.rpg.TextHelper;
-import cz.neumimto.rpg.configuration.Localization;
-import cz.neumimto.rpg.inventory.Armor;
-import cz.neumimto.rpg.inventory.CannotUseItemReson;
-import cz.neumimto.rpg.inventory.Weapon;
 import cz.neumimto.rpg.players.IActiveCharacter;
 import cz.neumimto.rpg.utils.ItemStackUtils;
 import org.spongepowered.api.Sponge;
@@ -17,11 +12,8 @@ import org.spongepowered.api.item.inventory.Inventory;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.Slot;
 import org.spongepowered.api.item.inventory.entity.Hotbar;
-import org.spongepowered.api.item.inventory.equipment.EquipmentTypes;
 import org.spongepowered.api.item.inventory.property.SlotIndex;
 import org.spongepowered.api.item.inventory.query.QueryOperationTypes;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.chat.ChatTypes;
 
 import java.util.List;
 import java.util.Optional;
@@ -72,63 +64,6 @@ public class DefaultPlayerInvHandler extends PlayerInvHandler {
         }
     }
 
-    //TODO second hand
-    protected void initializeHotbar(IActiveCharacter character, int slot, Slot s, Hotbar hotbar) {
-        int selectedSlotIndex = hotbar.getSelectedSlotIndex();
-        //very stupid but fast
-        Optional<ItemStack> peek = s.peek();
-
-        //unequip
-        HotbarObject hotbarObject = character.getHotbar()[slot];
-        if (hotbarObject != null) {
-            hotbarObject.onUnEquip(character);
-        }
-
-        //parse slot
-        if (peek.isPresent()) {
-            ItemStack itemStack1 = peek.get();
-
-            HotbarObject hotbarObject0 = inventoryService().getHotbarObject(character, itemStack1);
-
-            if (hotbarObject0 != HotbarObject.EMPTYHAND_OR_CONSUMABLE) {
-                hotbarObject0.setSlot(slot);
-                character.getHotbar()[slot] = hotbarObject0;
-                CannotUseItemReson reason = inventoryService().canUse(itemStack1, character);
-                //cannot use
-                if (reason != CannotUseItemReson.OK) {
-                    character.getHotbar()[slot] = hotbarObject0;
-                    character.getPlayer().sendMessage(ChatTypes.ACTION_BAR, TextHelper.parse(Localization.PLAYER_CANT_USE_HOTBAR_ITEMS));
-                    character.getDenyHotbarSlotInteractions()[slot] = true;
-                    return;
-                } else {
-                    character.getDenyHotbarSlotInteractions()[slot] = false;
-                }
-
-                //charm is active anywhere in the hotbar
-                if (hotbarObject0.getHotbarObjectType() == HotbarObjectTypes.CHARM) {
-                    hotbarObject0.onEquip(character);
-                    character.getDenyHotbarSlotInteractions()[slot] = false;
-                } else if (hotbarObject0.getHotbarObjectType() == HotbarObjectTypes.WEAPON && slot == selectedSlotIndex) {
-                    //weapon active only if item is in hand
-                    hotbarObject0.onRightClick(character); //simulate player interaction to equip the weapon
-                    ((Weapon) hotbarObject0).setCurrent(true);
-                }
-
-            } else {
-                //consumable/non weapon item
-                character.getHotbar()[slot] = HotbarObject.EMPTYHAND_OR_CONSUMABLE;
-                character.getDenyHotbarSlotInteractions()[slot] = false;
-            }
-
-        } else {
-            //empty slot
-            character.getHotbar()[slot] = HotbarObject.EMPTYHAND_OR_CONSUMABLE;
-            character.getDenyHotbarSlotInteractions()[slot] = false;
-        }
-        // ??
-        initializeSecondHand(character);
-    }
-
     protected void initializeSecondHand(IActiveCharacter character) {
         HotbarObject offHand = character.getOffHand();
         if (offHand != null) {
@@ -144,102 +79,6 @@ public class DefaultPlayerInvHandler extends PlayerInvHandler {
         }
     }
 
-    @Override
-    public void initializeArmor(IActiveCharacter character) {
-        Optional<ItemStack> chestplate = character.getPlayer().getChestplate();
-        ItemStack is = null;
-        if (chestplate.isPresent()) {
-            is = chestplate.get();
-            CannotUseItemReson reason = inventoryService().canWear(is, character);
-            if (reason != CannotUseItemReson.OK) {
-                character.getPlayer().setChestplate(null);
-                inventoryService().dropItem(character, is, reason);
-            } else {
-
-                Armor armor = inventoryService().getChestplate(character);
-
-                Armor armor1 = character.getEquipedArmor().get(EquipmentTypes.CHESTPLATE);
-                if (armor1 != null) {
-                    effectService().removeGlobalEffectsAsEnchantments(armor1.getEffects().keySet(), character, armor1);
-                }
-                character.getEquipedArmor().put(EquipmentTypes.CHESTPLATE, armor);
-                effectService().applyGlobalEffectsAsEnchantments(armor.getEffects(), character, armor);
-
-            }
-        }
-
-        Optional<ItemStack> helmet = character.getPlayer().getHelmet();
-        if (helmet.isPresent()) {
-            is = helmet.get();
-            CannotUseItemReson reason = inventoryService().canWear(is, character);
-            if (reason != CannotUseItemReson.OK) {
-                character.getPlayer().setHelmet(null);
-                inventoryService().dropItem(character, is, reason);
-            } else {
-
-                Armor armor = inventoryService().getHelmet(character);
-
-                Armor armor1 = character.getEquipedArmor().get(EquipmentTypes.HEADWEAR);
-                if (armor1 != null) {
-                    effectService().removeGlobalEffectsAsEnchantments(armor1.getEffects().keySet(), character, armor1);
-                }
-                character.getEquipedArmor().put(EquipmentTypes.HEADWEAR, armor);
-                effectService().applyGlobalEffectsAsEnchantments(armor.getEffects(), character, armor);
-
-            }
-        }
-        Optional<ItemStack> boots = character.getPlayer().getBoots();
-        if (boots.isPresent()) {
-            is = boots.get();
-            CannotUseItemReson reason = inventoryService().canWear(is, character);
-            if (reason != CannotUseItemReson.OK) {
-                character.getPlayer().setBoots(null);
-                inventoryService().dropItem(character, is, reason);
-            } else {
-                Armor armor = inventoryService().getBoots(character);
-                Armor armor1 = character.getEquipedArmor().get(EquipmentTypes.BOOTS);
-                if (armor1 != null) {
-                    effectService().removeGlobalEffectsAsEnchantments(armor1.getEffects().keySet(), character, armor1);
-                }
-                character.getEquipedArmor().put(EquipmentTypes.BOOTS, armor);
-                effectService().applyGlobalEffectsAsEnchantments(armor.getEffects(), character, armor);
-            }
-        }
-        Optional<ItemStack> leggings = character.getPlayer().getLeggings();
-        if (leggings.isPresent()) {
-            is = leggings.get();
-            CannotUseItemReson reason = inventoryService().canWear(is, character);
-            if (reason != CannotUseItemReson.OK) {
-                character.getPlayer().setLeggings(null);
-
-                inventoryService().dropItem(character, is, reason);
-            } else {
-                Armor armor = inventoryService().getLeggings(character);
-
-                Armor armor1 = character.getEquipedArmor().get(EquipmentTypes.LEGGINGS);
-                if (armor1 != null) {
-                    effectService().removeGlobalEffectsAsEnchantments(armor1.getEffects().keySet(), character, armor1);
-                }
-                character.getEquipedArmor().put(EquipmentTypes.LEGGINGS, armor);
-                effectService().applyGlobalEffectsAsEnchantments(armor.getEffects(), character, armor);
-            }
-        }
-    }
-
-    @Override
-    public void changeActiveHotbarSlot(IActiveCharacter character, int slot) {
-        if (character.getDenyHotbarSlotInteractions()[slot]) {
-            HotbarObject hotbarObject = character.getHotbar()[slot];
-            if (hotbarObject != HotbarObject.EMPTYHAND_OR_CONSUMABLE) {
-                if (hotbarObject.getHotbarObjectType() == HotbarObjectTypes.WEAPON) {
-                   dropItemFromMainHand(character, slot);
-                }
-                character.getPlayer().sendMessage(ChatTypes.ACTION_BAR, TextHelper.parse(Localization.CANNOT_USE_ITEM_GENERIC));
-            }
-        } else {
-            character.getPlayer().sendMessage(ChatTypes.ACTION_BAR, Text.of(""));
-        }
-    }
 
     @Override
     public void onRightClick(IActiveCharacter character, int slot) {
