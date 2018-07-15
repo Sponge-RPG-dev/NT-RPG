9f100e5cdb00d21537d9db012e03835864d0c8c8
NeumimTo <chleba48@gmail.com>
2018-07-16 00:00:26 +0200

    update

---
+++ Plugin/src/main/java/cz/neumimto/rpg/skills/PassiveSkillFactory.java
@@ -0,0 +1,13 @@
+package cz.neumimto.rpg.skills;
+
+import cz.neumimto.rpg.effects.IGlobalEffect;
+
+/**
+ * Created by NeumimTo on 15.7.2018.
+ */
+public class PassiveSkillFactory {
+
+    public void static generate(String skillId, String skillName, Class<? extends IGlobalEffect> effect) {
+
+    }
+}
