34d6c265eec61bcefe46601411620dc1e5d4f60c
NeumimTo <chleba48@gmail.com>
2018-08-25 18:44:19 +0200

    why is this still even here

--- anunnamedproject/src/main/java/cz/neumimto/dei/entity/database/structure/Reward.java
+++
@@ -1,7 +0,0 @@
-package cz.neumimto.dei.entity.database.structure;
-
-/**
- * Created by ja on 31.7.2016.
- */
-public class Reward {
-}
--- anunnamedproject/src/main/java/cz/neumimto/dei/serivce/ClaimedAreaType.java
+++
@@ -1,8 +0,0 @@
-package cz.neumimto.dei.serivce;
-
-/**
- * Created NeumimTo ja on 6.7.2016.
- */
-public enum ClaimedAreaType {
-	STRONGHOLD, TOWN;
-}
