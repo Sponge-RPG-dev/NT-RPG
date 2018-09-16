77abc55e70ef442b2520c4b949161caef3a738e6
NeumimTo <chleba48@gmail.com>
2018-09-16 11:38:05 +0200

    testing

--- Plugin/src/main/test/cz/neumimto/rpg/GuiceTest.java
+++ Plugin/src/main/test/cz/neumimto/rpg/GuiceTest.java
@@ -13,6 +13,10 @@ public class GuiceTest {
 	public void test() {
 		Injector injector = Guice.createInjector();
 		injector.getInstance(A.class).callBWhichCallsA();
+		A instance = injector.getInstance(A.class);
+		//is it com.sun.Proxy?
+		System.out.println(instance.getClass().getCanonicalName());
+		System.out.println(instance.b == injector.getInstance(B.class));
 	}
 
 }
