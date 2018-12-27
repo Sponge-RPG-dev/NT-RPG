7cc1ddcad9a83908f40ae6df3a7b0664c69a206b
NeumimTo <chleba48@gmail.com>
2018-12-27 21:49:50 +0100

    port one more skill from multitalleted`s skillpack

---
+++ Skills/src/main/java/cz/neumimto/skills/active/DropTNT.java
@@ -0,0 +1,61 @@
+package cz.neumimto.skills.active;
+
+import cz.neumimto.core.ioc.Inject;
+import cz.neumimto.rpg.ResourceLoader;
+import cz.neumimto.rpg.entities.EntityService;
+import cz.neumimto.rpg.players.IActiveCharacter;
+import cz.neumimto.rpg.skills.ExtendedSkillInfo;
+import cz.neumimto.rpg.skills.SkillNodes;
+import cz.neumimto.rpg.skills.SkillResult;
+import cz.neumimto.rpg.skills.SkillSettings;
+import cz.neumimto.rpg.skills.mods.SkillContext;
+import cz.neumimto.rpg.skills.parents.ActiveSkill;
+import cz.neumimto.rpg.skills.tree.SkillType;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Created by NeumimTo on 27.12.2018.
+ */
+@ResourceLoader.Skill("ntrpg:droptnt")
+public class DropTNT extends ActiveSkill {
+
+    public static Map<UUID, Float> DROPPED_TNTS = new HashMap<>();
+
+    @Inject
+    private EntityService entityService;
+
+    public void init() {
+        super.init();
+        SkillSettings settings = new SkillSettings();
+        settings.addNode(SkillNodes.AMOUNT, 1f ,0f);
+        settings.addNode(SkillNodes.DAMAGE, 100f ,10f);
+        settings.addNode("explosion-radius", 3 ,0.1f);
+        setSettings(settings);
+        addSkillType(SkillType.PHYSICAL);
+        addSkillType(SkillType.SUMMON);
+        setIcon(ItemTypes.TNT);
+    }
+
+    @Override
+    public void cast(IActiveCharacter character, ExtendedSkillInfo info, SkillContext skillContext) {
+        Player player = character.getPlayer();
+        Location<World> location = player.getLocation();
+        World extent = location.getExtent();
+        Entity tnt = extent.createEntity(EntityTypes.PRIMED_TNT, location.getPosition());
+        int i = skillContext.getIntNodeValue("explosion-radius");
+        tnt.offer(Keys.EXPLOSION_RADIUS, Optional.of(i));
+        extent.spawnEntity(tnt);
+        skillContext.next(character, info, SkillResult.OK);
+    }
+}
