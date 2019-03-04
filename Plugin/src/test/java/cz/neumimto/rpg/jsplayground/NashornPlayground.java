66d181cd0e66e6a169a47baecb245eceff0807bd
NeumimTo <chleba48@gmail.com>
2019-03-04 20:26:14 +0100

    and fix the test

---
+++ Plugin/src/test/java/cz/neumimto/rpg/jsplayground/NashornPlayground.java
@@ -0,0 +1,37 @@
+package cz.neumimto.rpg.jsplayground;
+
+import jdk.internal.dynalink.beans.StaticClass;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import java.io.File;
+import java.nio.file.Files;
+
+public class NashornPlayground {
+
+
+
+    @Test
+    public void test0() throws Exception {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("playground/test01.js").getFile());
+
+        ScriptEngine scriptEngine = new NashornScriptEngineFactory().getScriptEngine("--optimistic-types=true"/*, "-d=bytecode/"*/);
+        Bindings bindings = scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
+        byte[] bytes = Files.readAllBytes(file.toPath());
+        Object eval = scriptEngine.eval(new String(bytes));
+        ScriptObjectMirror mirror = (ScriptObjectMirror) scriptEngine.eval("array");
+        StaticClass o = (StaticClass) mirror.get("0");
+        StaticClass o1 = (StaticClass) mirror.get("1");
+        Assert.assertSame(o1, o);
+        Assert.assertSame(o1.getRepresentedClass(), o.getRepresentedClass());
+        StaticClass o2 = (StaticClass) mirror.get("2");
+        Assert.assertNotSame(o1, o2);
+        int i = 0;
+    }
+}
