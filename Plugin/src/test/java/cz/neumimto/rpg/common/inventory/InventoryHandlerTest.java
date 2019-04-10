9e4691c1848904551619f4a544dd94f5e3df3bcb
NeumimTo <chleba48@gmail.com>
2019-04-10 19:43:22 +0200

    test

--- Plugin/src/test/java/cz/neumimto/rpg/common/inventory/InventoryHandlerTest.java
+++ Plugin/src/test/java/cz/neumimto/rpg/common/inventory/InventoryHandlerTest.java
@@ -1,5 +1,6 @@
 package cz.neumimto.rpg.common.inventory;
 
+import cz.neumimto.rpg.api.items.RpgItemStack;
 import cz.neumimto.rpg.api.items.WeaponClass;
 import cz.neumimto.rpg.common.items.RpgItemStackImpl;
 import cz.neumimto.rpg.common.items.TestItemType;
@@ -9,11 +10,17 @@ import cz.neumimto.rpg.junit.TestGuiceModule;
 import cz.neumimto.rpg.players.IActiveCharacter;
 import name.falgout.jeffrey.testing.junit.guice.GuiceExtension;
 import name.falgout.jeffrey.testing.junit.guice.IncludeModule;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.inject.Inject;
 import java.util.Collections;
+import java.util.stream.Stream;
 
 import static cz.neumimto.rpg.junit.CharactersExtension.Stage;
 import static cz.neumimto.rpg.junit.CharactersExtension.Stage.Stages.READY;
@@ -25,17 +32,34 @@ class InventoryHandlerTest {
     @Inject
     private InventoryHandler inventoryHandler;
 
+    private IActiveCharacter character;
 
-    @Test
-    void handleCharacterEquipActionPre(@Stage(READY) IActiveCharacter character) {
+    @BeforeEach
+    public void beforeEach(@Stage(READY) IActiveCharacter character) {
+        this.character = character;
+    }
+
+    @ParameterizedTest
+    @MethodSource("methodProvider01")
+    void handleCharacterEquipActionPre(RpgItemStack testItemStack, int i, boolean result) {
         boolean mayUse = inventoryHandler.handleCharacterEquipActionPre(character,
-                character.getManagedInventory().getManagedSlots().get(1),
-                new RpgItemStackImpl(
-                        new TestItemType("itemid", null, new WeaponClass("*"), 10, 0),
-                        Collections.emptyMap(),
-                        Collections.emptyMap(),
-                        Collections.emptyMap()
-                ));
+                character.getManagedInventory().getManagedSlots().get(i),
+                testItemStack);
+
+        Assertions.assertSame(mayUse, result);
+    }
+
+
+
+    private static Stream<Arguments> methodProvider01() {
+
+        return Stream.of(
+                Arguments.of(new RpgItemStackImpl(new TestItemType("itemid", null, new WeaponClass("*"), 10, 0),
+                                    Collections.emptyMap(), Collections.emptyMap(), Collections.emptyMap()),1, true) ,
+                Arguments.of(new RpgItemStackImpl(new TestItemType("itemid", null, new WeaponClass("1"), 10, 0),
+                                    Collections.emptyMap(), Collections.emptyMap(), Collections.emptyMap()),0, false)
+        );
+
     }
 
     @Test
