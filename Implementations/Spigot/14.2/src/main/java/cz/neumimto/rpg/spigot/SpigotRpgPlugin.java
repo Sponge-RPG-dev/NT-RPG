be344979f7ba76593128917399274efa73230e79
NeumimTo <chleba48@gmail.com>
2019-09-08 22:24:07 +0200

    Start testing 2.1.0

--- Implementations/Spigot/14.2/src/main/java/cz/neumimto/rpg/spigot/SpigotRpgPlugin.java
+++ Implementations/Spigot/14.2/src/main/java/cz/neumimto/rpg/spigot/SpigotRpgPlugin.java
@@ -1,12 +1,12 @@
 package cz.neumimto.rpg.spigot;
 
 import co.aikar.commands.PaperCommandManager;
-import com.google.common.collect.ImmutableList;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import cz.neumimto.rpg.api.Rpg;
-import cz.neumimto.rpg.api.effects.IEffectService;
+import cz.neumimto.rpg.api.RpgAddon;
 import cz.neumimto.rpg.api.effects.IGlobalEffect;
+import cz.neumimto.rpg.common.AddonScanner;
 import cz.neumimto.rpg.spigot.commands.SpigotAdminCommands;
 import cz.neumimto.rpg.spigot.resources.SpigotGuiceModule;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,6 +14,9 @@ import org.bukkit.plugin.java.annotation.plugin.*;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 @Plugin(name = "NT-RPG", version = "0.0.1-SNAPSHOT")
 @Description("Complete combat overhaul with classes and skills")
@@ -33,7 +36,32 @@ public class SpigotRpgPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        Injector injector = Guice.createInjector(new SpigotGuiceModule(this, Collections.emptyMap()));
+        AddonScanner.setAddonDir(getDataFolder().toPath().resolve("addons"));
+        AddonScanner.setDeployedDir(getDataFolder().toPath().resolve(".deployed"));
+
+        AddonScanner.prepareAddons();
+        AddonScanner.onlyReloads();
+
+        Set<Class<?>> classesToLoad = AddonScanner.getClassesToLoad();
+        Iterator<Class<?>> iterator = classesToLoad.iterator();
+
+        Map extraBindings = Collections.emptyMap();
+
+        while (iterator.hasNext()) {
+            Class<?> next = iterator.next();
+            if (RpgAddon.class.isAssignableFrom(next)) {
+                try {
+                    RpgAddon addon = (RpgAddon) next.getConstructor().newInstance();
+                    extraBindings = addon.getBindings();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                iterator.remove();
+            }
+        }
+
+        Injector injector = Guice.createInjector(new SpigotGuiceModule(this, extraBindings));
+        Rpg.get().getResourceLoader().initializeComponents();
         PaperCommandManager manager = new PaperCommandManager(this);
 
         manager.getCommandContexts().registerIssuerAwareContext(IGlobalEffect.class, c -> {
---
+++ Implementations/Spigot/14.2/src/main/java/cz/neumimto/rpg/spigot/commands/SpigotCharacterCommands.java
@@ -0,0 +1,54 @@
+package cz.neumimto.rpg.spigot.commands;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Subcommand;
+import cz.neumimto.rpg.api.entity.players.IActiveCharacter;
+import cz.neumimto.rpg.api.gui.Gui;
+import cz.neumimto.rpg.common.commands.CharacterCommandFacade;
+import cz.neumimto.rpg.spigot.SpigotRpgPlugin;
+import cz.neumimto.rpg.spigot.entities.players.SpigotCharacterService;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.UUID;
+
+@Singleton
+@CommandAlias("char|c")
+public class SpigotCharacterCommands extends BaseCommand {
+
+
+    @Inject
+    private CharacterCommandFacade characterCommandFacade;
+
+    @Inject
+    private SpigotCharacterService characterService;
+
+    @Inject
+    private SpigotRpgPlugin plugin;
+
+    @Subcommand("list")
+    public void characterList(Player executor) {
+        IActiveCharacter character = characterService.getCharacter(executor);
+        Gui.sendListOfCharacters(character, character.getCharacterBase());
+    }
+
+    @Subcommand("create")
+    public void createCharacter(Player executor, String name) {
+        UUID uuid = executor.getUniqueId();
+
+        characterCommandFacade.commandCreateCharacter(uuid, name, actionResult -> {
+            executor.sendMessage(actionResult.getMessage());
+        });
+    }
+
+    @Subcommand("switch")
+    public void deleteCharacter(Player executor, String name) {
+        IActiveCharacter character = characterService.getCharacter(executor);
+        characterCommandFacade.commandSwitchCharacter(character, name, runnable -> {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, runnable);
+        });
+    }
+}
