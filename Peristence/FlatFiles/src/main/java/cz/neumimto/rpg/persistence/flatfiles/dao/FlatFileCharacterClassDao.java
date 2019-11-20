59361e68b29fc6a0d5b5c791f99c375198c5facb
NeumimTo <chleba456@gmaill.com>
2019-11-21 00:24:50 +0100

    flat file storage update

--- Peristence/FlatFiles/src/main/java/cz/neumimto/rpg/persistence/flatfiles/dao/FlatFileCharacterClassDao.java
+++ Peristence/FlatFiles/src/main/java/cz/neumimto/rpg/persistence/flatfiles/dao/FlatFileCharacterClassDao.java
@@ -1,4 +1,20 @@
 package cz.neumimto.rpg.persistence.flatfiles.dao;
 
-public class FlatFileCharacterClassDao {
+import cz.neumimto.rpg.api.persistance.model.CharacterClass;
+import cz.neumimto.rpg.common.persistance.dao.ICharacterClassDao;
+import cz.neumimto.rpg.common.persistance.dao.IPlayerDao;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class FlatFileCharacterClassDao implements ICharacterClassDao {
+
+    @Inject
+    private IPlayerDao playerDao;
+
+    @Override
+    public void update(CharacterClass characterClass) {
+        playerDao.update(characterClass.getCharacterBase());
+    }
 }
