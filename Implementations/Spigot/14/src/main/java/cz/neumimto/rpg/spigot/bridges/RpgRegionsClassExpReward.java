b914a322781f67710e2e109dc0c9edba269f4703
NeumimTo <chleba456@gmaill.com>
2020-01-31 22:31:04 +0100

    Integration into RpgRegions

---
+++ Implementations/Spigot/14/src/main/java/cz/neumimto/rpg/spigot/bridges/RpgRegionsClassExpReward.java
@@ -0,0 +1,33 @@
+package cz.neumimto.rpg.spigot.bridges;
+
+import cz.neumimto.rpg.api.Rpg;
+import cz.neumimto.rpg.api.entity.players.IActiveCharacter;
+import net.islandearth.rpgregions.rewards.DiscoveryReward;
+import org.bukkit.entity.Player;
+
+public class RpgRegionsClassExpReward extends DiscoveryReward {
+
+    public static final String SOURCE = "RPG_REGIONS";
+
+    private double exp;
+
+    public RpgRegionsClassExpReward(double exp) {
+        this.exp = exp;
+    }
+
+    public RpgRegionsClassExpReward() {
+    }
+
+    @Override
+    public void award(Player player) {
+        IActiveCharacter character = Rpg.get().getCharacterService().getCharacter(player.getUniqueId());
+        if (!character.isStub()) {
+            Rpg.get().getCharacterService().addExperiences(character, exp, SOURCE);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "ClassExpReward";
+    }
+}
