681ad326ae0bbcf7828a56e239f09025e35be467
NeumimTo <chleba48@gmail.com>
2020-05-15 21:36:58 +0200

    added dummy spell

---
+++ Common/src/main/java/cz/neumimto/rpg/common/skills/NullAndVoidSkill.java
@@ -0,0 +1,20 @@
+package cz.neumimto.rpg.common.skills;
+
+import cz.neumimto.rpg.api.ResourceLoader;
+import cz.neumimto.rpg.api.entity.players.IActiveCharacter;
+import cz.neumimto.rpg.api.skills.PlayerSkillContext;
+import cz.neumimto.rpg.api.skills.SkillResult;
+import cz.neumimto.rpg.api.skills.types.ActiveSkill;
+
+@ResourceLoader.Skill("ntrpg:null_and_void")
+public class NullAndVoidSkill extends ActiveSkill {
+    @Override
+    public SkillResult cast(IActiveCharacter character, PlayerSkillContext info) {
+        return SkillResult.OK;
+    }
+
+    @Override
+    public SkillResult onPreUse(Object character, PlayerSkillContext esi) {
+        return SkillResult.OK;
+    }
+}
