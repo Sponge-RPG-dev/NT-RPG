d02b8efdc4d433f5b46ca61febd74132e0e726be
NeumimTo <chleba48@gmail.com>
2021-02-14 10:55:09 +0100

    fix offsets in ascii map

--- initSpigotServ.sh
+++ initSpigotServ.sh
@@ -8,7 +8,7 @@ mkdir -p $PLUGINS
 function dl() {
   curl -LO ${2} --output-dir ${1}
 }
-dl $SPATH/server-spigot/ https://papermc.io/api/v1/paper/1.16.4/314/download
+dl $SPATH/server-spigot/ https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/468/downloads/paper-1.16.5-468.jar
 [ -f $SPATH/server-spigot/download ] && mv $SPATH/server-spigot/download $SPATH/server-spigot/server.jar
 dl $PLUGINS https://github.com/tr7zw/Item-NBT-API/releases/download/2.6.0/item-nbt-api-plugin-2.6.0.jar
 dl $PLUGINS https://media.forgecdn.net/files/3108/83/EffectLib-6.4.jar
